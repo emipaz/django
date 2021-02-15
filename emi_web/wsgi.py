b7e50ddb7ba1eb701458b759af8fba80926f7170
emiliano <emipaz1975@hotmail.com>
2021-02-15 15:00:56 +0000

    Agregamos la app polls

--- emi_web/wsgi.py
+++ emi_web/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
+
 
 import os
 
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emi_web.settings')
 
 application = get_wsgi_application()
+
+"""
+####
+import os
+import sys
+
+path = os.path.expanduser('~/emi_web/emi_web')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'emi_web.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())

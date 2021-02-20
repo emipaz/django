6e7de68a1cc42b536fbb6eb5770b1c3245bb193e
emiliano <emipaz1975@hotmail.com>
2021-02-20 16:30:11 +0000

    primera parte del tutorial part 4

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_object_or_404, render
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 
-from .models import Question
+from .models import Question , Choice
 
-from django.template import loader
+# from django.template import loader
 
-from django.shortcuts import render
+from django.urls import reverse
+
+from django.http import Http404
+
 
 def index(request):
     ################ primrera vista version con Httresponse #################
@@ -28,10 +31,6 @@ def index(request):
     ##### Version utilizando el metodo render del modulo django.shortcuts #####
     return render(request, 'polls/index.html', contexto)
 
-
-from django.http import Http404
-
-
 def detail(request, question_id):
     # primera version sin exepciones
     # return HttpResponse("You're looking at question %s." % question_id)
@@ -48,12 +47,39 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
+
+def owner(request):
+       return HttpResponse("Hello, world. 7c738d98 is the polls index.")
+
+
+
+"""
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+"""
 
-def owner(request):
-       return HttpResponse("Hello, world. 7c738d98 is the polls index.")
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html',
+                               {'question'      : question,
+                                'error_message' : "You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Siempre devuelva un HttpResponseRedirect después de negociar con éxito
+        # con datos POST. Esto evita que los datos se publiquen dos veces si un
+        # el usuario presiona el botón Atrás.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})

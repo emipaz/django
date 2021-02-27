dfe060293ef018b8cc3ec1bce561f0c02613285a
emiliano <emipaz1975@hotmail.com>
2021-02-27 01:20:33 +0000

    Agregar imágenes a la aplicación de anuncios

--- ads/admin.py
+++ ads/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from ads.models import Ad
+
+class PicAdmin(admin.ModelAdmin):
+    exclude = ('picture', 'content_type')
+
+
 # Register your models here.
 admin.site.register(Ad)
--- ads/views.py
+++ ads/views.py
@@ -1,28 +1,89 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render , redirect, get_object_or_404
 
-# Create your views here.
+from django.urls import reverse_lazy
+
+from django.http import HttpResponse
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from ads.models import Ad
+
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+from ads.forms import CreateForm
 
 class AdListView(OwnerListView):
     model = Ad
     # By convention:
-    template_name = "myarts/article_list.html"
-
+    template_name = "ads/ad_list.html"
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = "ads/ad_detail.html"
 
+class AdCreateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads:all')
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ["title","text","price"]
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
 
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ["title","text","price"]
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
 
+        # Add owner to the model before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+class AdUpdateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk):
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=pic)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        pic = form.save(commit=False)
+        pic.save()
+
+        return redirect(self.success_url)
+
+#class AdCreateView(OwnerCreateView):
+#    model = Ad
+#    fields = ["title","text","price"]
+
+#class AdUpdateView(OwnerUpdateView):
+#    model = Ad
+#    fields = ["title","text","price"]
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
+
+def stream_file(request, pk):
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
